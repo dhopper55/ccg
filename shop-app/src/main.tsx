ecffbe5647f307805008a5b3ac276a243733ae09
David Hopper <david@turbogarage.com>
2026-03-31 10:38:48 -0600

    New Claude Shop stuff 4

--- shop-app/src/main.tsx
+++ shop-app/src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import BreakpointsProvider from 'providers/BreakpointsProvider';
 import NotistackProvider from 'providers/NotistackProvider';
+import SettingsPanelProvider from 'providers/SettingsPanelProvider';
 import SettingsProvider from 'providers/SettingsProvider';
 import ThemeProvider from 'providers/ThemeProvider';
 import router from 'routes/router';
@@ -11,9 +12,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <SettingsProvider>
     <ThemeProvider>
       <NotistackProvider>
-        <BreakpointsProvider>
-          <RouterProvider router={router} />
-        </BreakpointsProvider>
+        <SettingsPanelProvider>
+          <BreakpointsProvider>
+            <RouterProvider router={router} />
+          </BreakpointsProvider>
+        </SettingsPanelProvider>
       </NotistackProvider>
     </ThemeProvider>
   </SettingsProvider>,
