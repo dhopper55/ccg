a745cf12d477e39b2aaaf37f2236914b1061f574
David Hopper <davidhopper55@gmail.com>
2026-06-08 09:43:18 -0600

    eval page to shop 2

--- shop-app/src/pages/guitar-eval/GuitarEvalPage.tsx
+++ shop-app/src/pages/guitar-eval/GuitarEvalPage.tsx
@@ -1,29 +1,33 @@
 import { Paper } from '@mui/material';
 import Grid from '@mui/material/Grid';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import MainLayout from 'layouts/main-layout';
 import AddContactStepper from 'components/sections/crm/add-contact/AddContactStepper';
 import PageHeader from 'components/sections/ecommerce/admin/common/PageHeader';
 
 const GuitarEvalPage = () => {
   return (
-    <MainLayout>
-      <Grid container>
-        <Grid size={12}>
-          <PageHeader
-            title="$ Coal Creek Guitar Evaluation Report"
-            breadcrumb={[
-              { label: 'Home', url: 'https://www.coalcreekguitars.com' },
-              { label: 'Guitar Evaluation', active: true },
-            ]}
-          />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <MainLayout>
+        <Grid container>
+          <Grid size={12}>
+            <PageHeader
+              title="$ Coal Creek Guitar Evaluation Report"
+              breadcrumb={[
+                { label: 'Home', url: 'https://www.coalcreekguitars.com' },
+                { label: 'Guitar Evaluation', active: true },
+              ]}
+            />
+          </Grid>
+          <Grid size={12}>
+            <Paper sx={{ p: { xs: 3, md: 5 } }}>
+              <AddContactStepper />
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid size={12}>
-          <Paper sx={{ p: { xs: 3, md: 5 } }}>
-            <AddContactStepper />
-          </Paper>
-        </Grid>
-      </Grid>
-    </MainLayout>
+      </MainLayout>
+    </LocalizationProvider>
   );
 };
 
