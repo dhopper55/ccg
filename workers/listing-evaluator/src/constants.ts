3b681f7965bb019c77b0973cf91bd9f31c5ff279
David Hopper <davidhopper55@gmail.com>
2026-09-01 21:13:04 -0600

    latest public changes

--- workers/listing-evaluator/src/constants.ts
+++ workers/listing-evaluator/src/constants.ts
@@ -33,6 +33,8 @@ export const SITEMAP_STATIC_URLS = [
   { loc: '/about-us.html', changefreq: 'monthly', priority: '0.6' },
   { loc: '/guitar-value-report-evaluation', changefreq: 'monthly', priority: '0.7' },
   { loc: '/guitar-authenticity-report', changefreq: 'monthly', priority: '0.7' },
+  { loc: '/we-buy-music-gear-denver-englewood/', changefreq: 'monthly', priority: '0.8' },
+  { loc: '/denver-area-music-gear-consignment/', changefreq: 'monthly', priority: '0.8' },
   { loc: '/how-to-list-a-guitar-for-sale.html', changefreq: 'monthly', priority: '0.7' },
   { loc: '/fender-guitar-serial-number-history.html', changefreq: 'monthly', priority: '0.7' },
   { loc: '/gibson-guitar-serial-number-history.html', changefreq: 'monthly', priority: '0.7' },
