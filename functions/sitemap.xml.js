d72000a6b716ec2dc9f18291aea940e8af630284
David Hopper <davidhopper55@gmail.com>
2026-09-15 21:41:13 -0600

    finance page 2

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -36,6 +36,7 @@ const STATIC_URLS = [
   { loc: '/guitar-value-report-evaluation', changefreq: 'monthly', priority: '0.7' },
   { loc: '/we-buy-music-gear-denver-englewood/', changefreq: 'monthly', priority: '0.8' },
   { loc: '/denver-area-music-gear-consignment/', changefreq: 'monthly', priority: '0.8' },
+  { loc: '/musical-gear-financing/', changefreq: 'monthly', priority: '0.8' },
   { loc: '/fender-guitar-serial-number-history.html', changefreq: 'monthly', priority: '0.7' },
   { loc: '/gibson-guitar-serial-number-history.html', changefreq: 'monthly', priority: '0.7' },
   { loc: '/ccg-englewood-broadway-gothic-theatre-history.html', changefreq: 'monthly', priority: '0.6' },
